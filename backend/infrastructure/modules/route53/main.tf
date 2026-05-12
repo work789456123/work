54275b27f3b0e178827fa04d2ec53303590dc998
hridesh-net <hridyaash0811@gmail.com>
2026-05-12 08:49:11 -0400

    terraform update

--- backend/infrastructure/modules/route53/main.tf
+++ backend/infrastructure/modules/route53/main.tf
@@ -14,3 +14,17 @@ resource "aws_route53_record" "app_alias" {
     evaluate_target_health = true
   }
 }
+
+resource "aws_route53_record" "additional_alias" {
+  for_each = toset(var.additional_record_names)
+
+  zone_id = data.aws_route53_zone.selected.zone_id
+  name    = each.value
+  type    = "A"
+
+  alias {
+    name                   = var.alb_dns_name
+    zone_id                = var.alb_zone_id
+    evaluate_target_health = true
+  }
+}
